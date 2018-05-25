231afe46a1401bfdd3b32bf0053beeb83da6c974
federico.rosato@supsi.ch <federico.rosato@supsi.ch>
2018-05-25 14:51:12 +0200

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'D:\GDrive\Pycharm\krangpower'))
+sys.path.insert(0, os.path.abspath(r'../..'))
 
 
 # -- General configuration ------------------------------------------------
