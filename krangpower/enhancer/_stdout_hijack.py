367e6c615e83737341276f948bb8c36a1c92e8f6
federico.rosato@supsi.ch <federico.rosato@supsi.ch>
2018-09-26 08:31:47 +0200

    Bug fix

--- krangpower/enhancer/_stdout_hijack.py
+++ krangpower/enhancer/_stdout_hijack.py
@@ -16,19 +16,21 @@ class stdout_redirected_linux:
 
     def __enter__(self):
         self.stdout_fileno = sys.stdout.fileno()
-        self.stdout_save = os.dup(self.stdout_fileno)
+        self.stdout_save = os.dup(self.stdout_fileno)  # refers to the pre-existent stdout file
         self.stdout_pipe = os.pipe()
-        os.dup2(self.stdout_pipe[1], self.stdout_fileno)
-        os.close(self.stdout_pipe[1])
+        os.dup2(self.stdout_pipe[1], self.stdout_fileno)  # hijacks stdout fileno's handler to the pipe's write end
+        os.close(self.stdout_pipe[1])  # we need no more the open pipe's write end
 
-        self.t = threading.Thread(target=drain_pipe, args=(self.stdout_pipe[0],))
+        self.t = threading.Thread(target=self.drain_pipe, args=(self.stdout_pipe[0],))
         self.t.start()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        os.close(self.stdout_fileno)
+        os.close(self.stdout_fileno)  # this triggers the thread's exit condition
         self.t.join()
-        os.close(self.stdout_pipe[0])
-        os.dup2(self.stdout_save, self.stdout_fileno)
+
+        # clean up
+        os.close(self.stdout_pipe[0])  # pipe's read end
+        os.dup2(self.stdout_save, self.stdout_fileno)  # resets stdout to the original file handler
         os.close(self.stdout_save)
 
 
